55a4fcdd4980433c587be024ac4c485509178e3c
Robert Kerth <rob.kerth@gmail.com>
2018-03-18 21:01:51 -0400

    Database setup script

---
+++ skynet_setup.sql
@@ -0,0 +1,12 @@
+﻿--DROP TABLE tokens;
+CREATE TABLE tokens (token varchar(60) primary key, authorized_uses integer);
+
+GRANT SELECT ON tokens TO lambda_client;
+
+--DROP TABLE results;
+CREATE TABLE results (result_id varchar(60) primary key, time_inserted timestamp, result_value text);
+
+GRANT INSERT, SELECT ON results TO lambda_client;
+
+CREATE INDEX idx_tokens ON tokens(token);
+CREATE INDEX idx_results ON results(result_id);
